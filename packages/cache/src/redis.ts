19dfb31777d7405deb19b386c1ad0ea4085ea586
Ayaan Shaikh <63299939+IamAyaanSk@users.noreply.github.com>
2024-09-17 02:09:12 +0530

    fix: removed hardcoded urls

--- packages/cache/src/redis.ts
+++ packages/cache/src/redis.ts
@@ -1,6 +1,5 @@
 import { Redis } from 'ioredis'
-const REDIS_CONNECTION_URL =
-  process.env.REDIS_CONNECTION_URL || 'redis://localhost:6379'
+const REDIS_CONNECTION_URL = process.env.REDIS_CONNECTION_URL || ''
 const redis = new Redis(REDIS_CONNECTION_URL)
 
 redis.on('connecting', () => {
